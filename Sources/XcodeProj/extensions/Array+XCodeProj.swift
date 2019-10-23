25b6f2bee9834c8dd4144ec051d13731f3b51e1f
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-10-23 15:03:46 -0400

    Added internal appending methods to return new array including existing and new elements Added new has and removing methods when Element is XcodeProjectBuilders.DefaultDetailsChoice to simplify removing specific XcodeProjectBuilders.DefaultDetailsChoice in other parts of the code

--- Sources/XcodeProj/extensions/Array+XCodeProj.swift
+++ Sources/XcodeProj/extensions/Array+XCodeProj.swift
@@ -15,3 +15,51 @@ internal extension Array {
         return rtn
     }
 }
+
+internal extension Array where Element == XcodeProjectBuilders.DefaultDetailsChoice {
+    /// Checks to see if a given choice/option is contained within the array
+    /// - Parameter option: The choice(s)/option(s) to check for
+    func has(_ option: XcodeProjectBuilders.DefaultDetailsChoice) -> Bool {
+        for element in self {
+            if element.has(option) { return true }
+        }
+        return false
+    }
+    
+    /// Removes any choices/options provides from the given array
+    /// - Parameter options: The choice(s)/option(s) to remove
+    /// - Returns: A newly created array with the leftover choice(s)/option(s)
+    func removing(_ options: [XcodeProjectBuilders.DefaultDetailsChoice]) -> [XcodeProjectBuilders.DefaultDetailsChoice] {
+        var rtn: [XcodeProjectBuilders.DefaultDetailsChoice] = self
+        /// Remove any items in lhs that exist in rhs
+        var idx: Int = 0
+        while idx < rtn.count {
+            if options.has(rtn[idx]) {
+                rtn.remove(at: idx)
+            } else {
+                idx += 1
+            }
+        }
+        return rtn
+    }
+    
+    /// Removes any choices/options provides from the given array
+    /// - Parameter option: The choice(s)/option(s) to remove
+    /// - Returns: A newly created array with the leftover choice(s)/option(s)
+    func removing(_ option: XcodeProjectBuilders.DefaultDetailsChoice) -> [XcodeProjectBuilders.DefaultDetailsChoice] {
+        return self.removing([option])
+    }
+}
+
+internal extension Array {
+    func appending(_ newElement: Element) -> [Element] {
+        var rtn: [Element] = self
+        rtn.append(newElement)
+        return rtn
+    }
+    func appending<S>(contentsOf newElements: S) -> [Element] where Element == S.Element, S : Sequence {
+        var rtn: [Element] = self
+        rtn.append(contentsOf: newElements)
+        return rtn
+    }
+}
