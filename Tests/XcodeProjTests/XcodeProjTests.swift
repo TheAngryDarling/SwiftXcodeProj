10e4419f42bce454f50cfb471169d8450aff7e56
Tyler Anger <30189019+TheAngryDarling@users.noreply.github.com>
2019-12-09 05:33:17 -0500

    Added test testAddAndRemoveFilesFromSwiftProject to test adding and removing references and file system objects

--- Tests/XcodeProjTests/XcodeProjTests.swift
+++ Tests/XcodeProjTests/XcodeProjTests.swift
@@ -1,6 +1,7 @@
 import XCTest
 import CodeTimer
 @testable import XcodeProj
+import PBXProj
 import SwiftPatches
 
 #if os(Linux)
@@ -329,36 +330,319 @@ class XcodeProjTests: XCTestCase {
         }
     }
     
-    func testAddFileToSwiftProject() {
-        let workingDir: URL = URL(fileURLWithPath: NSTemporaryDirectory())
-        let workingProjectName: String = "TestSwiftCommandLine"
-        let projectFolder: URL = workingDir.appendingPathComponent(workingProjectName, isDirectory: true)
-        let sourcesFolder =  projectFolder.appendingPathComponent("Sources", isDirectory: true)
-        let targetFolder = sourcesFolder.appendingPathComponent(workingProjectName, isDirectory: true)
-        defer {
-            try? FileManager.default.removeItem(at: projectFolder)
-        }
-        do {
-                   
-           let proj = try XcodeProjectBuilders.Swift.CommandLine.create(workingProjectName,
-                                                                        in: workingDir,
-                                                                        havingUserDetails: .testUserDetails)
-            let testFile: URL = URL(fileURLWithPath: #file)
-            let destTestFile: URL = targetFolder.appendingPathComponent(testFile.lastPathComponent)
+    func testAddAndRemoveFilesFromSwiftProject() {
+         let workingDir: URL = URL(fileURLWithPath: NSTemporaryDirectory())
+         let workingProjectName: String = "TestSwiftCommandLine"
+         let groupName: String = "SubGroup"
+         let projectFolder: URL = workingDir.appendingPathComponent(workingProjectName, isDirectory: true)
+         let sourcesFolder =  projectFolder.appendingPathComponent("Sources", isDirectory: true)
+         let targetFolder = sourcesFolder.appendingPathComponent(workingProjectName, isDirectory: true)
+         
+         defer {
+             try? FileManager.default.removeItem(at: projectFolder)
+         }
+         // Clear out any unclean project
+         try? FileManager.default.removeItem(at: projectFolder)
+        
+         do {
+                    
+            var proj = try XcodeProjectBuilders.Swift.CommandLine.create(workingProjectName,
+                                                                         in: workingDir,
+                                                                         havingUserDetails: .testUserDetails)
+            
+             let projectPackagePath = proj.projectPackage
+             let testFile: URL = URL(fileURLWithPath: #file)
+             
+
+             var targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+             var subGroup = try targetGroup.createGroup(withName: groupName)
+            
+            // Create copy file destination URL
+            let destTestFile: URL = subGroup.fullURL.url.appendingPathComponent(testFile.lastPathComponent)
+            
             /// Copy test file into project
             try FileManager.default.copyItem(at: testFile, to: destTestFile)
-
-            let targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
-            let localResourceFile = targetGroup.fullURL.appendingFileComponent(testFile.lastPathComponent)
             
-            try targetGroup.addExisting(localResourceFile,
-                                           includeInTargets: [proj.targets.first!],
-                                           copyLocally: true,
-                                           savePBXFile: false)
-           print(proj)
-       } catch {
-           XCTFail("\(error)")
-       }
+             let localResourceFile = subGroup.fullURL.appendingFileComponent(testFile.lastPathComponent)
+             
+             var testSrcFile = try subGroup.addExisting(localResourceFile,
+                                                        includeInTargets: [proj.targets.first!],
+                                                        copyLocally: true,
+                                                        savePBXFile: true)
+            
+            // Check to make sure build rule has been added
+            guard proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                XCTFail("Unable to find build file for source '\(testSrcFile.name)'")
+                return
+            }
+            
+            //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Setup initial project") }
+            
+            // Trying to remove reference to file
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                guard let f = subGroup.file(atPath: testFile.lastPathComponent) else {
+                    XCTFail("Unable to locate file '\(testFile.lastPathComponent))'")
+                    return
+                }
+                
+                try f.remove(deletingFiles: false, savePBXFile: true)
+                // Verify file still exists on file system
+                guard try proj.fsProvider.itemExists(at: f.fullURL) else {
+                    XCTFail("File '\(testFile.lastPathComponent))' was deleted when expecting reference only be removed")
+                    return
+                }
+                // Checking to make sure the build file rule was removed
+                guard !proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Found build file reference for '\(testSrcFile.name)' after file was removed")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Removed file reference") }
+            }
+            
+            // Adding test file back to project
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                guard subGroup.file(atPath: testFile.lastPathComponent) == nil else {
+                    XCTFail("Found file '\(testFile.lastPathComponent))' when expecting not to")
+                    return
+                }
+                
+                let localResourceFile = subGroup.fullURL.appendingFileComponent(testFile.lastPathComponent)
+                
+                testSrcFile = try subGroup.addExisting(localResourceFile,
+                                                       includeInTargets: [proj.targets.first!],
+                                                       copyLocally: true,
+                                                       savePBXFile: true)
+                
+                // Check to make sure build rule has been added
+                guard proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Unable to find build file for source '\(testSrcFile.name)'")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Added file reference back") }
+            }
+            // Trying to remove folder reference and all child references
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                guard let f = subGroup.file(atPath: testFile.lastPathComponent) else {
+                    XCTFail("Unable to locate file '\(testFile.lastPathComponent))'")
+                    return
+                }
+                
+                try subGroup.remove(deletingFiles: false, savePBXFile: true)
+                
+                // Making sure group file reference has been removed
+                guard targetGroup.group(atPath: groupName) == nil else {
+                    XCTFail("Expected not to find group '\(subGroup.name))' after deleting")
+                    return
+                }
+                // Making sure file reference has been remvoed
+                guard targetGroup.resource(atPath: "\(groupName)/\(testFile.lastPathComponent)") == nil else {
+                    XCTFail("Expected not to find file '\(testFile.lastPathComponent))' after deleting")
+                    return
+                }
+                
+                // Checking to make sure the build file rule was removed
+                guard !proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Found build file reference for '\(testSrcFile.name)' after file was removed")
+                    return
+                }
+                
+                // Verify folder still exists on file system
+                guard try proj.fsProvider.itemExists(at: subGroup.fullURL) else {
+                    XCTFail("Folder '\(subGroup.name))' was deleted when expecting reference only be removed")
+                    return
+                }
+                // Verify file still exists on file system
+                guard try proj.fsProvider.itemExists(at: f.fullURL) else {
+                    XCTFail("File '\(testFile.lastPathComponent))' was deleted when expecting reference only be removed")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Remvoed folder and child references back") }
+            }
+            
+            // Adding test group and file back to project
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                guard targetGroup.group(atPath: groupName) == nil else {
+                    XCTFail("Found group '\(groupName)' when expecting not to")
+                    return
+                }
+                let localResourceFile = targetGroup.fullURL.appendingDirComponent(groupName)
+                try targetGroup.addExisting(localResourceFile,
+                                            includeInTargets: [proj.targets.first!],
+                                            copyLocally: true,
+                                            savePBXFile: true)
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                
+                guard let f = subGroup.file(atPath: testFile.lastPathComponent) else {
+                    XCTFail("Unable to locate file '\(testFile.lastPathComponent))'")
+                    return
+                }
+                testSrcFile = f
+                
+                // Checking to make sure the build file rule was removed
+                guard proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Unable to find build file for source '\(testSrcFile.name)'")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Added folder and child references back") }
+                
+                
+            }
+            
+            // Deleting test file reference and physical file
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                guard let f = subGroup.file(atPath: testFile.lastPathComponent) else {
+                    XCTFail("Unable to locate file '\(testFile.lastPathComponent))'")
+                    return
+                }
+                testSrcFile = f
+                
+                try f.remove(deletingFiles: true, savePBXFile: true)
+                
+                // Checking to make sure the build file rule was removed
+                guard !proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Found build file reference for '\(testSrcFile.name)' after file was removed")
+                    return
+                }
+                
+                
+                // Verify file is gone
+                guard !(try proj.fsProvider.itemExists(at: f.fullURL)) else {
+                    XCTFail("File '\(testFile.lastPathComponent))' was NOT deleted when expecting reference and file removed")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Deleted file reference and file") }
+                
+            }
+            
+            // Copy test file back into project
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                
+                // Making sure file reference has been removed
+                guard subGroup.file(atPath: testFile.lastPathComponent) == nil else {
+                    XCTFail("Expected not to find file '\(testFile.lastPathComponent))' after deleting")
+                    return
+                }
+                
+                // Create copy file destination URL
+                let destTestFile: URL = subGroup.fullURL.url.appendingPathComponent(testFile.lastPathComponent)
+                
+                /// Copy test file into project
+                try FileManager.default.copyItem(at: testFile, to: destTestFile)
+                
+                let localResourceFile = subGroup.fullURL.appendingFileComponent(testFile.lastPathComponent)
+                 
+                 let testSrcFile = try subGroup.addExisting(localResourceFile,
+                                                            includeInTargets: [proj.targets.first!],
+                                                            copyLocally: true,
+                                                            savePBXFile: true)
+                
+                // Check to make sure build rule has been added
+                guard proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Unable to find build file for source '\(testSrcFile.name)'")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Re-added test file") }
+                
+            }
+            
+            // Delete subgroup and child files
+            do {
+                proj = try XcodeProject(fromURL: projectPackagePath.url)
+                targetGroup = proj.resources.group(atPath: "Sources/\(workingProjectName)")!
+                
+                guard let grp = targetGroup.group(atPath: groupName) else {
+                    XCTFail("Unable to locate subgroup '\(groupName)'")
+                    return
+                }
+                subGroup = grp
+                
+                guard let f = subGroup.file(atPath: testFile.lastPathComponent) else {
+                    XCTFail("Unable to locate file '\(testFile.lastPathComponent))'")
+                    return
+                }
+                testSrcFile = f
+                
+                try subGroup.remove(deletingFiles: true, savePBXFile: true)
+                
+                // Checking to make sure the build file rule was removed
+                guard !proj.targets.first!.sourcesBuildPhase().files.contains(where: { $0.fileRef == testSrcFile.pbxFileResource.id }) else {
+                    XCTFail("Found build file reference for '\(testSrcFile.name)' after file was removed")
+                    return
+                }
+                
+                
+                // Verify file is gone
+                guard !(try proj.fsProvider.itemExists(at: f.fullURL)) else {
+                    print("File: " + f.fullURL.url.path)
+                    XCTFail("File '\(testFile.lastPathComponent))' was NOT deleted when expecting reference and file removed")
+                    return
+                }
+                
+                // Verify folder is gone
+                guard !(try proj.fsProvider.itemExists(at: subGroup.fullURL)) else {
+                    print("Folder: " + subGroup.fullURL.url.path)
+                    XCTFail("Folder '\(testFile.lastPathComponent))' was NOT deleted when expecting reference and folder removed")
+                    return
+                }
+                
+                //if isXcodeTesting { print("testAddAndRemoveFilesFromSwiftProject: Deleted sub group and child files") }
+            }
+            
+        } catch {
+            XCTFail("\(error)")
+        }
     }
 
     func testEmpty() { }
@@ -371,6 +655,7 @@ class XcodeProjTests: XCTestCase {
         ("testOtherProjects", testOtherProjects),
         ("testCreateSwiftCommandLineApp", testCreateSwiftCommandLineApp),
         ("testCreateObjCCommandLineApp", testCreateObjCCommandLineApp),
-        ("testCreateOtherEmptyProject", testCreateOtherEmptyProject)
+        ("testCreateOtherEmptyProject", testCreateOtherEmptyProject),
+        ("testAddAndRemoveFilesFromSwiftProject", testAddAndRemoveFilesFromSwiftProject)
     ]
 }
